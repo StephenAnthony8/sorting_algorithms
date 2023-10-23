5b675c75ba15998098cb332fda95f7ee3e53aba3
StephenAnthony8 <steveoti.tony@gmail.com>
2023-10-24 00:24:20 +0300

    Sorting algorithms and Big O

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,15 +9,15 @@
  */
 int max_int(int *array, int size)
 {
-	int max, i;
+	int max_val, i;
 
-	max = array[0];
+	max_val = array[0];
 	for (i = 1; i < size; i++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		if (array[i] > max_val)
+			max_val = array[i];
 	}
-	return (max);
+	return (max_val);
 }
 /**
  * counting_sort - sorts an array of ints using
@@ -27,9 +27,10 @@ int max_int(int *array, int size)
  */
 void counting_sort(int *array, size_t size)
 {
-	int max, i;
+	int max, i, size_int;
 	int *counting_array, *sorted_array;
 
+	size_int = (int)size;
 	if (array == NULL || size < 2)
 		return;
 	max = max_int(array, size);
@@ -45,17 +46,17 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i < (max + 1); i++)
 		counting_array[i] = 0;
 
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size_int; i++)
 		counting_array[array[i]] += 1;
 	for (i = 0; i < (max + 1); i++)
 		counting_array[i] += counting_array[i - 1];
 	print_array(counting_array, max + 1);
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size_int; i++)
 	{
 		sorted_array[counting_array[array[i]] - 1] = array[i];
 		counting_array[array[i]] -= 1;
 	}
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size_int; i++)
 		array[i] = sorted_array[i];
 
 	free(sorted_array);
